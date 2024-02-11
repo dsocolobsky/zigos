6f0c980b27bc67d8332da9bce0a302207a0e3c2c
Dylan Socolobsky <dsocolobsky@gmail.com>
2024-02-11 00:42:01 -0300

    somewhat working, black screen

--- build.zig
+++ build.zig
@@ -4,71 +4,57 @@ const builtin = @import("builtin");
 const Build = std.Build;
 const Arch = std.Target.Cpu.Arch;
 const CrossTarget = std.zig.CrossTarget;
-const Target = @import("std").Target;
 
 pub fn build(b: *Build) !void {
-    const target = genTarget();
+    const target = try genTarget();
 
-    const kernel = buildKernel(b, target);
-    const iso = buildLimineIso(b, kernel);
-    b.default_step.dependOn(iso);
-    _ = runIsoQemu(b, iso);
+    const kernel = try buildKernel(b, target);
+    const iso = try buildLimineIso(b, kernel);
+    b.default_step.dependOn(&iso.step);
+    _ = try runIsoQemu(b, iso);
 }
 
-fn genTarget() CrossTarget {
-    const features = Target.x86.Feature;
-
-    var disabled_features = Target.Cpu.Feature.Set.empty;
-    var enabled_features = Target.Cpu.Feature.Set.empty;
-
-    disabled_features.addFeature(@intFromEnum(features.mmx));
-    disabled_features.addFeature(@intFromEnum(features.sse));
-    disabled_features.addFeature(@intFromEnum(features.sse2));
-    disabled_features.addFeature(@intFromEnum(features.avx));
-    disabled_features.addFeature(@intFromEnum(features.avx2));
-    enabled_features.addFeature(@intFromEnum(features.soft_float));
-
-    const target = CrossTarget{
-        .cpu_arch = Target.Cpu.Arch.x86_64,
-        .os_tag = Target.Os.Tag.freestanding,
-        .abi = Target.Abi.none,
-        .cpu_features_sub = disabled_features,
-        .cpu_features_add = enabled_features,
+fn genTarget() !CrossTarget {
+    var target = CrossTarget{
+        .cpu_arch = .x86_64,
+        .os_tag = .freestanding,
+        .abi = .none,
     };
+
+    const features = std.Target.x86.Feature;
+    target.cpu_features_sub.addFeature(@intFromEnum(features.x87));
+    target.cpu_features_sub.addFeature(@intFromEnum(features.mmx));
+    target.cpu_features_sub.addFeature(@intFromEnum(features.sse));
+    target.cpu_features_sub.addFeature(@intFromEnum(features.sse2));
+    target.cpu_features_sub.addFeature(@intFromEnum(features.avx));
+    target.cpu_features_sub.addFeature(@intFromEnum(features.avx2));
+    target.cpu_features_add.addFeature(@intFromEnum(features.soft_float));
 
     return target;
 }
 
-fn buildKernel(b: *Build, target: CrossTarget) *Build.Step {
+fn buildKernel(b: *Build, target: CrossTarget) !*Build.InstallArtifactStep {
     const optimize = b.standardOptimizeOption(.{});
 
-    var kernel = b.addExecutable(.{
+    const kernel = b.addExecutable(.{
         .name = "kernel",
         .root_source_file = .{ .path = "src/main.zig" },
-        .target = b.resolveTargetQuery(target),
-        .optimize = optimize,
-        .code_model = .kernel,
-    });
-    kernel.linker_script = .{ .path = "src/linker.ld" };
-
-    const limine = b.dependency("limine", .{
         .target = target,
         .optimize = optimize,
     });
-    kernel.addModule("limine", limine.module("limine"));
+    kernel.code_model = .kernel;
+    kernel.linker_script = .{ .path = "src/linker.ld" };
 
-    const art = b.addInstallArtifact(kernel, .{}).step;
-    b.getInstallStep().dependOn(art);
-    return &art;
+    // const limine = b.dependency("limine", .{
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
+    // kernel.addModule("limine", limine.module("limine"));
+
+    return b.addInstallArtifact(kernel, .{});
 }
 
-fn buildLimineIso(b: *Build, kernel: *Build.Step) *Build.Step {
-    const limine_deploy = switch (builtin.os.tag) {
-        .linux => "limine-deploy",
-        .windows => "limine-deploy.exe",
-        else => return error.UnsupportedOS,
-    };
-
+fn buildLimineIso(b: *Build, kernel: *Build.InstallArtifactStep) !*Build.RunStep {
     // TODO: use submodules with a hardcoded path until Zig supports
     //       static assets
     const limine_path = "external/limine";
@@ -79,24 +65,24 @@ fn buildLimineIso(b: *Build, kernel: *Build.Step) *Build.Step {
         try std.mem.concat(b.allocator, u8, &[_][]const u8{
             "mkdir -p zig-out/iso/root && ",
             "make -C " ++ limine_path ++ " && ",
-            "cp zig-out/bin/", "kernel.image", " zig-out/iso/root && ",
+            "cp zig-out/bin/", kernel.artifact.out_filename, " zig-out/iso/root && ",
             "cp src/limine.cfg zig-out/iso/root && ",
-            "cp ", limine_path ++ "/limine.sys ",
-                   limine_path ++ "/limine-cd.bin ",
-                   limine_path ++ "/limine-cd-efi.bin ",
+            "cp ", limine_path ++ "/limine-bios.sys ",
+                   limine_path ++ "/limine-bios-cd.bin ",
+                   limine_path ++ "/limine-uefi-cd.bin ",
                    "zig-out/iso/root && ",
             "xorriso -as mkisofs -quiet -b limine-cd.bin ",
                 "-no-emul-boot -boot-load-size 4 -boot-info-table ",
-                "--efi-boot limine-cd-efi.bin ",
+                "--efi-boot limine-uefi-cd.bin ",
                 "-efi-boot-part --efi-boot-image --protective-msdos-label ",
                 "zig-out/iso/root -o zig-out/iso/limine-barebones.iso && ",
-            limine_path ++ "/" ++ limine_deploy ++ " zig-out/iso/limine-barebones.iso",
+            limine_path ++ "/limine zig-out/iso/limine-barebones.iso",
         // zig fmt: on
         }),
     };
 
     const iso_cmd = b.addSystemCommand(cmd);
-    iso_cmd.step.dependOn(&kernel);
+    iso_cmd.step.dependOn(&kernel.step);
 
     const iso_step = b.step("iso", "Generate bootable ISO file");
     iso_step.dependOn(&iso_cmd.step);
@@ -104,7 +90,7 @@ fn buildLimineIso(b: *Build, kernel: *Build.Step) *Build.Step {
     return iso_cmd;
 }
 
-fn runIsoQemu(b: *Build, iso: *Build.Step) *Build.Step {
+fn runIsoQemu(b: *Build, iso: *Build.RunStep) !*Build.RunStep {
     const qemu_iso_args = &[_][]const u8{
         "qemu-system-x86_64",
         "-M",
@@ -117,7 +103,7 @@ fn runIsoQemu(b: *Build, iso: *Build.Step) *Build.Step {
         "d",
     };
     const qemu_iso_cmd = b.addSystemCommand(qemu_iso_args);
-    qemu_iso_cmd.step.dependOn(iso);
+    qemu_iso_cmd.step.dependOn(&iso.step);
 
     const qemu_iso_step = b.step("run", "Boot ISO in qemu");
     qemu_iso_step.dependOn(&qemu_iso_cmd.step);
--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,61 @@
 const std = @import("std");
 const builtin = @import("builtin");
-const limine = @import("limine");
 
 // The Limine requests can be placed anywhere, but it is important that
 // the compiler does not optimise them away, so, usually, they should
 // be made volatile or equivalent.
 
-pub export var framebuffer_request: limine.Framebuffer.Request = .{};
+const LimineVideoMode = extern struct {
+    pitch: u64,
+    width: u64,
+    height: u64,
+    bpp: u16,
+    memory_model: u8,
+    red_mask_size: u8,
+    red_mask_shift: u8,
+    green_mask_size: u8,
+    green_mask_shift: u8,
+    blue_mask_size: u8,
+    blue_mask_shift: u8,
+};
+
+const LimineFramebuffer = extern struct {
+    address: *anyopaque,
+    width: u64,
+    height: u64,
+    pitch: u64,
+    bpp: u64,
+    memory_model: u8,
+    red_mask_size: u8,
+    red_mask_shift: u8,
+    green_mask_size: u8,
+    green_mask_shift: u8,
+    blue_mask_size: u8,
+    blue_mask_shift: u8,
+    unused: [7]u8,
+    edid_size: u64,
+    edid: *anyopaque,
+    mode_count: u64,
+    modes: **LimineVideoMode,
+};
+
+const LimineFramebufferResponse = extern struct {
+    revision: u64,
+    framebuffer_count: u64,
+    framebuffers: **LimineFramebuffer,
+};
+
+const LimineFramebufferRequest = extern struct {
+    id: [4]u64,
+    revision: u64,
+    response: ?*LimineFramebufferResponse,
+};
+
+pub export var framebuffer_request: LimineFramebufferRequest = .{
+    .id = [4]u64{ 0x4c, 0x49, 0x4d, 0x49 },
+    .revision = 0,
+    .response = null,
+};
 
 // Halt and catch fire function
 fn hcf() void {
@@ -37,7 +86,7 @@ pub export fn _start() callconv(.C) void {
         }
 
         // Fetch the first framebuffer
-        const framebuffer = framebuffer_response.getFramebuffers()[0];
+        const framebuffer = *framebuffer_response.framebuffers[0];
 
         // Note: we assume the framebuffer model is RGB with 32-bit pixels.
         for (0..100) |i| {
