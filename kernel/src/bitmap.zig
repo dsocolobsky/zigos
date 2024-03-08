b50d1e0c580f9a117efc85da01e140603197a8f3
Dylan Socolobsky <dsocolobsky@gmail.com>
2024-03-08 16:35:36 -0300

    Complete bitmap page allocator

---
+++ kernel/src/bitmap.zig
@@ -0,0 +1,120 @@
+const std = @import("std");
+const serial = @import("serial.zig");
+
+const PAGE_SIZE = 4096;
+
+inline fn bit_is_set(n: u8, bit: u8) bool {
+    return (n & (@as(u8, 1) << @intCast(bit))) != 0;
+}
+
+inline fn set_bit(n: *u8, bit: u8) void {
+    n.* |= (@as(u8, 1) << @intCast(bit));
+}
+
+inline fn clear_bit(n: *u8, bit: u8) void {
+    n &= ~(1 << bit);
+}
+
+inline fn div_roundup(a: usize, b: usize) usize {
+    return (a + (b - 1)) / b;
+}
+
+pub const Bitmap = struct {
+    total_pages: usize = 0,
+    free_pages: usize = 0,
+    used_pages: usize = 0,
+    size: usize = 0,
+    map: [*]u8 = undefined,
+
+    const Self = @This();
+
+    pub fn init(start_of_map: usize, size: usize, total_pages: usize) Bitmap {
+        return Bitmap{
+            .total_pages = total_pages,
+            .free_pages = total_pages,
+            .used_pages = 0,
+            .size = size,
+            .map = @ptrFromInt(start_of_map),
+        };
+    }
+
+    pub fn log(self: *const Self) void {
+        serial.print("[bitmap] total_pages: {d} ({d}Mb)\n" ++
+            "[bitmap] free_pages: {d} ({d}Mb)\n" ++
+            "[bitmap] used_pages: {d} ({d}Mb)\n" ++
+            "[bitmap] Bitmap size: {d}b , {d}kb\n", .{
+            self.total_pages,
+            self.total_pages * 4 / 1024,
+            self.free_pages,
+            self.free_pages * 4 / 1024,
+            self.used_pages,
+            self.used_pages * 4 / 1024,
+            self.size,
+            self.size / 1024,
+        });
+    }
+
+    /// Sets all entries to used
+    pub fn set_all_used(self: *Self) void {
+        @memset(self.map[0..self.size], 0xFF);
+        self.free_pages = 0;
+        self.used_pages = self.total_pages;
+    }
+
+    /// Sets a range of memory (in usize) marking it as unavailable with 0xFF
+    pub fn set_used_range(self: *Self, start_addr: usize, size: usize) void {
+        self.set_range_to(start_addr, size, 0xFF);
+    }
+
+    /// Clears a range of memory (in usize) marking it as available with 0x00
+    pub fn clear_range(self: *Self, start_addr: usize, size: usize) void {
+        self.set_range_to(start_addr, size, 0x00);
+    }
+
+    /// Sets a range of memory (in usize) to a certain value (0x00 or 0xFF)
+    fn set_range_to(self: *Self, start_addr: usize, size: usize, value: u8) void {
+        const num_pages = @divFloor(size, PAGE_SIZE);
+        const start_page = @divFloor(start_addr, PAGE_SIZE);
+        const byte_index = @divFloor(start_page, 8);
+        const num_bytes = @divFloor(num_pages, 8);
+        serial.println(
+            "[bitmap] Setting {d} pages from page #{d} byte index #{d} total {d} bytes",
+            .{ num_pages, start_page, byte_index, num_bytes },
+        );
+        self.map[byte_index] = value;
+        //@memset(self.map[byte_index .. byte_index + 1], value);
+        if (value == 0x00) {
+            self.free_pages +|= num_pages;
+            self.used_pages -|= num_pages;
+        } else {
+            self.used_pages +|= num_pages;
+            self.free_pages -|= num_pages;
+        }
+    }
+
+    /// Returns a single 4Kb page
+    pub fn get_page(self: *Self) ?usize {
+        var byte_index: usize = 0;
+        while (byte_index < self.total_pages / 8) : (byte_index += 1) {
+            var b: *u8 = &self.map[byte_index];
+            if (b.* & 0xFF == 0xFF) continue;
+
+            // Found a byte with at least 1 page (bit) clear
+            var bit_index: u8 = 0;
+            while (bit_index < 8) : (bit_index += 1) {
+                if (!bit_is_set(b.*, bit_index)) {
+                    const page_num = byte_index * 8 + bit_index;
+                    const addr: usize = page_num * PAGE_SIZE;
+                    serial.println(
+                        "[bitmap] returning page #{d} at addr 0x{X}",
+                        .{ page_num, addr },
+                    );
+                    set_bit(b, bit_index);
+                    return addr;
+                }
+            }
+        }
+        serial.print_err("[bitmap] failed to get free page", .{});
+        return null;
+    }
+};
--- kernel/src/pmm.zig
+++ kernel/src/pmm.zig
@@ -1,6 +1,6 @@
 const limine = @import("limine");
 const serial = @import("serial.zig");
-const stdheap = @import("std").heap;
+const Bitmap = @import("bitmap.zig").Bitmap;
 
 const PAGE_SIZE = 4096;
 
@@ -20,6 +20,13 @@ inline fn div_roundup(a: usize, b: usize) usize {
     return (a + (b - 1)) / b;
 }
 
+inline fn align_up(value: usize, al: usize) usize {
+    return div_roundup(value, al) * al;
+}
+
+var offset: usize = undefined;
+var bitmap: Bitmap = undefined;
+
 pub fn init() void {
     const memmap_response = memmap_request.response.?;
     //if (memmap_response == 0) {
@@ -30,6 +37,8 @@ pub fn init() void {
     if (hhdm_response == null) {
         serial.print_err("[pmm] Failed to get hhdm response", .{});
     }
+
+    offset = hhdm_response.?.offset;
 
     var usable_pages: usize = 0;
     var reserved_pages: usize = 0;
@@ -50,6 +59,7 @@ pub fn init() void {
     }
 
     const highest_page_idx = highest_addr / PAGE_SIZE;
+    const bitmap_size = align_up(highest_page_idx / 8, PAGE_SIZE);
 
     serial.println(
         "[pmm] Highest Address: 0x{X} , Highest Page Index: {d}",
@@ -64,27 +74,58 @@ pub fn init() void {
         .{(reserved_pages * PAGE_SIZE) / 1024 / 1024},
     );
 
-    const FREELIST_RESERVED_SIZE = 1024;
-    var freelist_start: usize = undefined;
-
+    // Find the first entry suitable to store the page bitmap
+    var bitmap_start: usize = undefined;
     for (memmap_response.entries()) |entry| {
         if (entry.kind != limine.MemoryMapEntryType.usable) {
             continue;
         }
-
-        // Allocate the freelist in the first 1Kb of the first usable page
-        if (entry.length >= FREELIST_RESERVED_SIZE) {
-            freelist_start = entry.base;
-            entry.base += FREELIST_RESERVED_SIZE;
-            entry.length -= FREELIST_RESERVED_SIZE;
+        if (entry.length >= bitmap_size) {
+            bitmap_start = offset + entry.base;
+            entry.base += bitmap_size;
+            entry.length -= bitmap_size;
             break;
         }
 
-        serial.print_err("[pmm] did not find space for the freelist!", .{});
+        serial.print_err("[pmm] did not find space for the bitmap!", .{});
         return;
     }
     serial.println(
-        "[pmm] Freelist starts at: 0x{X} , has size of {d} bytes.",
-        .{ freelist_start, FREELIST_RESERVED_SIZE },
+        "[pmm] Bitmap starts at: 0x{X} , has size of {d} Kb.",
+        .{ bitmap_start, bitmap_size / 1024 },
     );
+
+    const total_pages = usable_pages + reserved_pages;
+    bitmap = Bitmap.init(bitmap_start, bitmap_size, total_pages);
+    bitmap.set_all_used();
+    bitmap.log();
+
+    // Mark all available regions as free in the bitmap
+    for (memmap_response.entries()) |entry| {
+        if (entry.kind != limine.MemoryMapEntryType.usable) {
+            continue;
+        }
+        serial.println(
+            "[pmm] Clearing range: 0x{X} of {d} bytes",
+            .{ entry.base, entry.length },
+        );
+        bitmap.clear_range(entry.base, entry.length);
+    }
+    bitmap.log();
+    serial.puts("Asking for 5 pages");
+    _ = bitmap.get_page();
+    _ = bitmap.get_page();
+    _ = bitmap.get_page();
+    _ = bitmap.get_page();
+    bitmap.log();
+}
+
+// Returns the first available 4Kb page
+pub fn page() usize {
+    const addr = offset + bitmap.page().?;
+    serial.println(
+        "[pmm] Returning page starting at 0x{X}",
+        .{addr},
+    );
+    return addr;
 }
